e44d1ed8b49f3c77dec1d03cb999389a010bcc16
lukrzak <lukrzak@wp.pl>
2023-10-10 22:05:33 +0200

    Added base url value

--- src/main/java/com/lukrzak/ByForest/ByForestApplication.java
+++ src/main/java/com/lukrzak/ByForest/ByForestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ByForestApplication {
 
+	public static final String BASE_URL = "/api/v1";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ByForestApplication.class, args);
 	}
