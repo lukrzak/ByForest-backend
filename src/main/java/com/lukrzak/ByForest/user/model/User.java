22f617997f65f1660f4d1900144dc978e4fa3415
lukrzak <lukrzak@wp.pl>
2023-10-11 12:38:59 +0200

    Updated user model

--- src/main/java/com/lukrzak/ByForest/user/model/User.java
+++ src/main/java/com/lukrzak/ByForest/user/model/User.java
@@ -13,6 +13,8 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String username;
+	private String login;
+	private String password;
+	private String email;
 
 }
