2f2222b1ed0dc120182491f53e8017bdd2f7e811
lukrzak <lukrzak@wp.pl>
2023-10-18 23:12:28 +0200

    Added definition for new tests. Reorganized test file

--- src/test/java/com/lukrzak/ByForest/user/UserApiTests.java
+++ src/test/java/com/lukrzak/ByForest/user/UserApiTests.java
@@ -1,6 +1,5 @@
 package com.lukrzak.ByForest.user;
 
-import com.lukrzak.ByForest.user.controller.DefaultUserController;
 import com.lukrzak.ByForest.user.dto.PostUserRequest;
 import com.lukrzak.ByForest.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -9,18 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.server.ResponseStatusException;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import java.nio.charset.StandardCharsets;
-import java.util.function.Consumer;
 
 import static com.lukrzak.ByForest.ByForestApplication.BASE_URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,26 +34,45 @@ public class UserApiTests {
 	@Container
 	@ServiceConnection
 	private final static MySQLContainer<?> database = new MySQLContainer<>(DockerImageName.parse(DATABASE_IMAGE))
-			.withDatabaseName("forest-test-database")
+			.withDatabaseName("users-test-database")
 			.withUsername("admin")
 			.withPassword("password");
 
 	@AfterEach
-	void clean(){
+	void clean() {
 		userRepository.deleteAll();
 	}
 
 	@Test
-	void testAddingUser(){
-		PostUserRequest userRequest = new PostUserRequest("login", "pass", "email@em.com");
-		PostUserRequest otherUser = new PostUserRequest("ll", "ppp", "em@em.com");
-		PostUserRequest userWithTakenCredentials = new PostUserRequest("login", "ppp", "emaaa@em.com");
+	void testAddingUser() {
+		PostUserRequest userRequest = new PostUserRequest("login", "Password123!", "email@em.com");
+		PostUserRequest otherUser = new PostUserRequest("lloogg", "Password123!", "emaaaa@em.com");
 
 		assertEquals("User " + userRequest + " saved successfully", getPostUserResponse(userRequest));
 		assertEquals("User " + otherUser + " saved successfully", getPostUserResponse(otherUser));
+		assertEquals(2, userRepository.findAll().size());
+	}
+
+	@Test
+	void testAddingUserWithTakenCredentials() {
+		PostUserRequest userRequest = new PostUserRequest("login", "pass", "email@em.com");
+		PostUserRequest userWithTakenCredentials = new PostUserRequest("login", "ppp", "emaaaa@em.com");
+
+		assertEquals("User " + userRequest + " saved successfully", getPostUserResponse(userRequest));
 		assertEquals("User with login: " + userWithTakenCredentials.getLogin()
 				+ " or email: " + userWithTakenCredentials.getEmail()
 				+ " already exists", getPostUserResponse(userWithTakenCredentials));
+		assertEquals(1, userRepository.findAll().size());
+	}
+
+	@Test
+	void testAddingUserWithPasswordHashing() {
+		// TODO
+	}
+
+	@Test
+	void testAddingUserWithIncorrectCredentials() {
+		// TODO
 	}
 
 	private String getPostUserResponse(PostUserRequest userRequest){
