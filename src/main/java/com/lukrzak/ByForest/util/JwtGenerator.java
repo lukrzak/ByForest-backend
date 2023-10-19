84678b855381f731a8b43c13cd1328f7a15b83e7
lukrzak <lukrzak@wp.pl>
2023-10-19 15:22:34 +0200

    Moved environment variables to .env file

--- src/main/java/com/lukrzak/ByForest/util/JwtGenerator.java
+++ src/main/java/com/lukrzak/ByForest/util/JwtGenerator.java
@@ -2,7 +2,10 @@ package com.lukrzak.ByForest.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -10,11 +13,17 @@ import java.security.Key;
 import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class JwtGenerator {
 
-	@Value("${jwt.key}")
 	private String SECRET_KEY;
+	private final Environment environment;
 	private final long JWT_EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
+	@PostConstruct
+	private void setKey(){
+		SECRET_KEY = environment.getProperty("jwt.key");
+	}
 
 	public String generateJwtToken(String subject){
 		return Jwts.builder()
